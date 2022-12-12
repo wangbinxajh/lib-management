09b3b17baa2cff1ed8fac6876896286d96176718
王斌 <wangbin@wangbindeMacBook-Pro.local>
2022-12-12 17:06:08 +0800

    data

---
+++ mysql20221129/library_retur.sql
@@ -0,0 +1,61 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for macos11 (x86_64)
+--
+-- Host: 127.0.0.1    Database: library
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `retur`
+--
+
+DROP TABLE IF EXISTS `retur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `retur` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `book_name` varchar(5000) DEFAULT NULL,
+  `book_no` varchar(5000) DEFAULT NULL,
+  `user_no` varchar(45) DEFAULT NULL COMMENT '用户卡号',
+  `user_name` varchar(45) DEFAULT NULL COMMENT '用户名称',
+  `user_phone` varchar(45) DEFAULT NULL COMMENT '用户联系方式',
+  `createtime` varchar(5000) DEFAULT NULL,
+  `updatetime` datetime DEFAULT CURRENT_TIMESTAMP,
+  `score` int DEFAULT NULL COMMENT '借书积分',
+  `status` varchar(45) DEFAULT '已借出' COMMENT '借书状态',
+  `days` int DEFAULT NULL COMMENT '借书天数',
+  `return_date` datetime DEFAULT NULL COMMENT '归还日期',
+  `real_date` datetime DEFAULT NULL COMMENT '真正的还书日期',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `retur`
+--
+
+LOCK TABLES `retur` WRITE;
+/*!40000 ALTER TABLE `retur` DISABLE KEYS */;
+/*!40000 ALTER TABLE `retur` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-29  9:25:58
